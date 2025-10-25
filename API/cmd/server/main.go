5cdc0736fcb2bb18302cc5b0326141a9017faa0e
Wagner Bolfe <shaper@live.com>
2025-10-24 22:08:26 -0300

    Added Swagger documentation

--- API/cmd/server/main.go
+++ API/cmd/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/wagnerbolfe/goexpert/API/configs"
+	_ "github.com/wagnerbolfe/goexpert/API/docs"
 	"github.com/wagnerbolfe/goexpert/API/internal/entity"
 	"github.com/wagnerbolfe/goexpert/API/internal/infra/database"
 	"github.com/wagnerbolfe/goexpert/API/internal/infra/webserver/handlers"
@@ -13,8 +17,21 @@ import (
 	"gorm.io/gorm"
 )
 
+// @title           Go Expert Course API
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Wagner Bolfe
+// @contact.email  shaper@live.com
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-	_, err := configs.LoadConfig(".")
+	loadConfig, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
@@ -29,14 +46,40 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
+	//r.Use(middleware.Logger)
+	r.Use(LogRequest)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", loadConfig.TokenAuth))
+	r.Use(middleware.WithValue("JwtExpiresIn", loadConfig.JwtExpiresIn))
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(loadConfig.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJwt)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	err = http.ListenAndServe(":8000", r)
 	if err != nil {
 		return
 	}
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
---
+++ API/internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,103 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/jwtauth"
+	"github.com/wagnerbolfe/goexpert/API/internal/dto"
+	"github.com/wagnerbolfe/goexpert/API/internal/entity"
+	"github.com/wagnerbolfe/goexpert/API/internal/infra/database"
+)
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(userDB database.UserInterface) *UserHandler {
+	return &UserHandler{
+		UserDB: userDB,
+	}
+}
+
+// GetJWT godoc
+// @Summary      Get a user JWT
+// @Description  Get a user JWT
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request   body     dto.GetJWTInput  true  "user credentials"
+// @Success      200  {object}  dto.GetJWTOutput
+// @Failure      404  {object}  Error
+// @Failure      500  {object}  Error
+// @Router       /users/generate_token [post]
+func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
+	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
+	jwtExpiresIn := r.Context().Value("JwtExpiresIn").(int)
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	_, tokenString, _ := jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
+	})
+	accessToken := dto.GetJWTOutput{AccessToken: tokenString}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(accessToken)
+	if err != nil {
+		return
+	}
+}
+
+// GetJWT godoc
+// @Summary      Get a user JWT
+// @Description  Get a user JWT
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request   body     dto.GetJWTInput  true  "user credentials"
+// @Success      200  {object}  dto.GetJWTOutput
+// @Failure      404  {object}  Error
+// @Failure      500  {object}  Error
+// @Router       /users/generate_token [post]
+func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		e := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(e)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		err2 := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(err2)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
